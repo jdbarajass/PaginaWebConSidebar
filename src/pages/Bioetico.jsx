1db0ce4d1f8984552b6e2cb8ad262881d0e7d20e
jdbarajass <jdbarajass@gmail.com>
2021-11-10 13:25:14 -0500

    Agregar pagina Bioetico terminada

--- src/pages/Bioetico.jsx
+++ src/pages/Bioetico.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import calculo1 from "media/calculo1.png";
+import calculo2 from "media/calculo2.png";
 
 const Bioetico = () => {
-    return <div>Pensamiento Bioetico</div>;
+    return (
+      <div className="bg-green-300 h-full text-center">
+        <div className="text-xl font-extrabold ">
+          <Link to="/perfil">
+            <i className="text-pink-400 fas fa-home cursor-pointer hover:text-indigo-500 p-2" />
+            Ir a mi Perfil
+          </Link>
+          <br />
+          Pensamiento Bioético
+        </div>
+        <div className="">
+          <iframe
+            src="https://docs.google.com/viewer?srcid=1xh3FpIhspQuPMSLrn2WjONn3M2sWO7rq&pid=explorer&efh=false&a=v&chrome=false&embedded=true"
+            width="100%"
+            height="680px"
+          ></iframe>
+        </div>
+        <div className="p-4">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/iC39HPGZLnI"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+    );
 }
-
 export default Bioetico
